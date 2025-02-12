a1809d3589563f75a418c9d219a3a3458760b672
makaha03 <makaha@berkeley.edu>
2025-02-12 14:33:56 -0800

    Create test.py
    new test script

---
+++ test.py
@@ -0,0 +1,16 @@
+import csv
+import time
+import numpy as np
+
+file = open('test.csv', newline = None)
+
+csvwriter = csv.writer(file, delimiter=',')
+
+csvwriter.writerow(["Time","Data"])
+
+for i in range(10):
+    now = time.time()
+    value = np.random.random()
+    csvwriter.writerow([now,value])
+    time.sleep([1])
+file.close()

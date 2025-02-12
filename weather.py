a29679a177e0cf5bb8c56df35e7042b2eb91a9d2
makaha03 <makaha@berkeley.edu>
2025-02-12 14:54:24 -0800

    Updated ver

--- weather.py
+++ weather.py
@@ -11,11 +11,15 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1013.25
 
-while True:
-    print("\nTemperature: %0.1f C" % bme680.temperature)
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
-    print("Altitude = %0.2f meters" % bme680.altitude)
+run_duration = 10
+starti_time = time.time()
+i=0
 
+print("Starting sensor readings at:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+while i<5:
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"[{current_time}] Temp: {bme680.temperature:.1f} C, Gas: {bme680.gas} ohm," f"Humidity: {bme680.relative_humidity:.1f}%")
+    i+=1
     time.sleep(2)
+print("completed")

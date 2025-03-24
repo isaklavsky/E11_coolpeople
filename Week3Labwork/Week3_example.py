503084bb05778741b90b8e6539f35921786317ff
Matti <matteosdamanias@berkeley.edu>
2025-03-24 16:59:35 -0700

    Week 3 File Cleanup
    Added folder for Week 3, and then added labwork into it. No data file for week 3 there is no data. Also added Code description in comment.

--- Week3Labwork/Week3_example.py
+++ Week3Labwork/Week3_example.py
@@ -1,8 +1,11 @@
 import adafruit_bme680
-print("Hello World")
-
 import time
 import board
+# Prints Data from BME680 sensor. Redundant, use Week4 Code to create
+# new BME 680 Table instead, or use Week4 Collect Data as is if you have
+# the PM25 sensor as well
+
+
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()   # uses board.SCL and board.SDA

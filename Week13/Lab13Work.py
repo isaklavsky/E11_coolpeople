261af1a402180adc797ecf2a1c6dcd7ee21699ee
isaklavsky <143663092+isaklavsky@users.noreply.github.com>
2025-04-23 14:48:48 -0700

    Lab13Work.py
    da beginning

--- Week13/Lab13Work.py
+++ Week13/Lab13Work.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from glob import glob
 
+full_data = pd.read_csv("Loc1_session1_group1_2025-04-16_16-05-24.csv")
+display(full_data)
+spectrum_data = pd.read_csv("Spectrum_Loc1_session1_group1_2025-04-16_16-05-24.csv")
+display(spectrum_data)
